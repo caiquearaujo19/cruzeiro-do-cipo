adb91cbfc43d4a4facad8c07b4e2550138266f53
Caique Araujo <cam@cesar.org.br>
2022-02-04 18:47:59 -0300

    Pequeno ajuste no arquivo sorters.js

--- src/utils/js-utils/sorters.js
+++ src/utils/js-utils/sorters.js
@@ -1,6 +1,5 @@
-const year = sessionStorage.getItem('year')
-
 export const orderListByGoals = (a, b) => {
+    let year = sessionStorage.getItem('year')
     if(a[year] && b[year]) {
         if(a[year].goals < b[year].goals) {return 1}
         if(b[year].goals < a[year].goals) {return -1}
@@ -9,6 +8,7 @@ export const orderListByGoals = (a, b) => {
 }
 
 export const orderListByAssists = (a, b) => {
+    let year = sessionStorage.getItem('year')
     if(a[year] && b[year]) {
         if(a[year].assists < b[year].assists) {return 1}
         if(b[year].assists < a[year].assists) {return -1}
@@ -17,6 +17,7 @@ export const orderListByAssists = (a, b) => {
 }
 
 export const orderListByCleanSheets = (a, b) => {
+    let year = sessionStorage.getItem('year')
     if(a[year] && b[year]) {
         if(a[year].goalkeeper.cleanSheets < b[year].goalkeeper.cleanSheets) {return 1}
         if(b[year].goalkeeper.cleanSheets < a[year].goalkeeper.cleanSheets) {return -1}
@@ -26,6 +27,7 @@ export const orderListByCleanSheets = (a, b) => {
 }
 
 const orderKeepersByMatches = (a, b) => {
+    let year = sessionStorage.getItem('year')
     if(a[year].goalkeeper.matches < b[year].goalkeeper.macthes) {return 1}
     if(b[year].goalkeeper.matches < a[year].goalkeeper.matches) {return -1}
     return 0
