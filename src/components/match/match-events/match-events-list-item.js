91ff4c355ddde49f3f023e5aa1a2f4158a57be63
Caíque Araújo <kiqaraujo97@gmail.com>
2023-03-27 13:33:31 -0300

    Ajustes nas assistência de uma partida

--- src/components/match/match-events/match-events-list-item.js
+++ src/components/match/match-events/match-events-list-item.js
@@ -12,7 +12,7 @@ export default function MatchEventsListItem({scorer, assist}) {
         <PlayerAvatar photo={scorer.photo} size={40}/>
         <div className="match-events-list-item__content__info">
           <div className="match-events-list-item__content__info__scorer-name">{scorer.name}</div>
-          {assist !== null && assist !== undefined ?
+          {assist && assist.name !== '-' ?
             <div className="match-events-list-item__content__info__assist-name">Assistência: {assist.name}</div>
           : null}
         </div>
